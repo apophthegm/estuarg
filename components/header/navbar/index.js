9cb010e6622980fdbb2e94e22e976dbbbb77169a
Diego Díaz <didimar2001@gmail.com>
2022-01-22 14:18:05 -0500

    navbar now is responsive

--- components/header/navbar/index.js
+++ components/header/navbar/index.js
@@ -2,6 +2,27 @@ import style from "styles/home.module.css";
 import Link from "next/link";
 
 const Navbar = () => {
+  // let scroll = 0;
+
+  const menuClicked = () => {
+    const menu = document.getElementById("menu");
+    const menuCtn = document.getElementById("menuCtn");
+    // const body = document.body;
+
+    menu.classList.toggle(`${style.menu}`);
+    menuCtn.classList.toggle(`${style.reveal}`);
+
+    // if (body.style.position === "fixed") {
+    //   body.style = "";
+    //   window.scroll(0, scroll);
+    // } else {
+    //   scroll = window.scrollY;
+    //   body.style.top = `-${scroll}px`;
+    //   body.style.height = `calc(100% + ${scroll}px)`;
+    //   body.style.position = "fixed";
+    // }
+  };
+
   return (
     <header className={style.header}>
       <Link href="/">
@@ -9,6 +30,26 @@ const Navbar = () => {
           <h1 className={style.logo}>EstuArg</h1>
         </a>
       </Link>
+      <div className={style.rotateContainer} onClick={menuClicked}>
+        <svg
+            id="menu"
+            className={style.rotate}
+            viewBox="0 0 33 33"
+            width="32"
+            height="32"
+          >
+            <g
+              stroke="var(--color-primary)"
+              fill="none"
+              strokeWidth="3"
+              strokeLinecap="round"
+            >
+              <path d="M3 10 L29 10" />
+              <path d="M3 22 L29 22" />
+            </g>
+          </svg>
+      </div>
+      <section id="menuCtn" className={style.navbarCtn}>
       <ul className={style.navbar}>
         <li>
           <Link href="/">
@@ -31,6 +72,7 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+      </section>
     </header>
   );
 };
