1edfd5199707cec02fb2d12a08ee5d154bb45088
Diego Díaz <didimar2001@gmail.com>
2022-01-26 19:07:58 -0500

    CEO v1 and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import "styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <Head>
+      <meta name="theme-color" content="var(--black)" />
+      <meta name="msapplication-TileColor" content="#181616)" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="Content-Language" content="es" />
+      <title>Estuarg: Estudia en Argentina</title>
+      <meta name="description" content="Somos una empresa vanguardista, liderada por jóvenes que brindan ayuda a todas esas personas que deseen estudiar en Argentina. ¡Tú preocúpate de luchar por tus sueños y nosotros de ayudarte a conseguirlos!" />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+  );
 }
 
 export default MyApp;
