f9c46d9d3c24176af43f09fa782adbaa3383ac6e
Diego Díaz <didimar2001@gmail.com>
2022-01-26 19:07:19 -0500

    adding lang="es" tag to html tag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/avif", "image/webp"]
   },
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es"
+  }
 };
