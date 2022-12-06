36e5e65866ad4c32739c6d2826c923f210122401
diecodev <diegoddiaz05@gmail.com>
2022-12-06 02:42:53 -0500

    Fix: grammar

--- components/motivate.tsx
+++ components/motivate.tsx
@@ -17,7 +17,7 @@ export const Motivate = (): ReactElement => {
             El mundo está cambiando y la educación no puede quedarse atrás. El mundo necesita profesionales que sepan adaptarse a los cambios y que tengan una visión global de los problemas. Los estudiantes que estudian en el extranjero tienen una ventaja competitiva frente a los demás, ya que adquieren habilidades que les permiten ser más creativos, innovadores y emprendedores.
           </p>
           <p className={styles.text}>
-            Por otro lado, Argetina cuenta con muchos destinos turísticos como lo son el obelisco, la flor metálica, el planetario, entre muchos otros y además de esto, su gente es bastante amable, por lo que encontrarás gente hermosa para conocer y lugares llamativos para decubrir.
+            Por otro lado, Argentina cuenta con muchos destinos turísticos como lo son el obelisco, la flor metálica, el planetario, entre muchos otros y además de esto, su gente es bastante amable, por lo que encontrarás gente hermosa para conocer y lugares llamativos para decubrir.
           </p>
           <p className={styles.title}>
             Además de esto, Argentina cuenta con muchas universidades reconocidas a nivel mundial, como lo son la UBA o la UNLP, por lo que el nivel de educación superior del país es algo muy a tener en cuenta.
